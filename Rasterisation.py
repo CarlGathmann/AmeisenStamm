2d102cf492b74639540684582039eddf21431186
CarlGathmann <46105283+CarlGathmann@users.noreply.github.com>
2021-07-09 12:30:21 +0200

    looks good

--- Rasterisation.py
+++ Rasterisation.py
@@ -2,14 +2,14 @@ import pygame as pg
 from pygame import Vector2 as vec
 from bresenham import bresenham
 from random import randint
-from pygame.constants import TIMER_RESOLUTION
+import numpy as np
 
-from pygame.transform import threshold
+
 
 WIDTH = 800
 HEIGHT = 800
 GRID = 32
-DISTANCE = WIDTH/GRID
+SQUARE = WIDTH/GRID
 
 BLACK = (0,0,0)
 GREY = (30,30,30)
@@ -17,7 +17,7 @@ WHITE = (255,255,255)
 RED = (255,0,0)
 BLUE = (0,102,225)
 GREEN = (255,255,0)
-FPS = 1
+FPS = 5
 
 clock = pg.time.Clock()
 disp = pg.display.set_mode((WIDTH,HEIGHT))
@@ -41,7 +41,7 @@ class Pixel():
 
 #use this to create points in self defined coord-system
 def create_point(x,y):
-    return vec(DISTANCE*x+DISTANCE/2,DISTANCE*y+DISTANCE/2)
+    return vec(SQUARE*x+SQUARE/2,SQUARE*y+SQUARE/2)
 
 #gets coodinates of points crossing the funktion between two points
 def bresenham_list(first_pos,second_pos):
@@ -49,20 +49,20 @@ def bresenham_list(first_pos,second_pos):
 
 #colorises a list of points so that the line has a color gradient from color_1 to color_2
 def colorise_line(list,color1,color2):
-    length = len(list)-1
+    length = len(list)
     new_list = []
     for i in range(length):
         r = (((length-i)/length) * color1[0]) + (((0+i)/length) * color2[0])
         g = (((length-i)/length) * color1[1]) + (((0+i)/length) * color2[1])
         b = (((length-i)/length) * color1[2]) + (((0+i)/length) * color2[2])
-        pixel = Pixel(list[i],(r,g,b),DISTANCE-1)
+        pixel = Pixel(list[i],(r,g,b),SQUARE-1)
         new_list.append(pixel)
         pixel_list.append(pixel)
     return new_list
 
 #gets the linear funktion between two points
 def get_function(first_pos, second_pos):
-    x1, x2, y1, y2 = (first_pos.x-DISTANCE/2)/DISTANCE, (second_pos.x-DISTANCE/2)/DISTANCE, (first_pos.y-DISTANCE/2)/DISTANCE, (second_pos.y-DISTANCE/2)/DISTANCE
+    x1, x2, y1, y2 = (first_pos.x-SQUARE/2)/SQUARE, (second_pos.x-SQUARE/2)/SQUARE, (first_pos.y-SQUARE/2)/SQUARE, (second_pos.y-SQUARE/2)/SQUARE
     m = (y2-y1)/(x2-x1)
     n = -m*x1+y1
     return m,n        
@@ -70,11 +70,9 @@ def get_function(first_pos, second_pos):
 pg.init()
 
 #points to get going
-FIRST_POINT = Pixel(create_point(randint(0,GRID-1),randint(0,GRID-1)), RED,DISTANCE-0.5)
-SECOND_POINT = Pixel(create_point(randint(0,GRID-1),randint(0,GRID-1)),BLUE,DISTANCE-0.5)
-THIRD_POINT = Pixel(create_point(randint(0,GRID-1),randint(0,GRID-1)),GREEN,DISTANCE-0.5)
-
-print(DISTANCE)
+FIRST_POINT = Pixel(create_point(randint(0,GRID-1),randint(0,GRID-1)), RED,SQUARE-0.5)
+SECOND_POINT = Pixel(create_point(randint(0,GRID-1),randint(0,GRID-1)),BLUE,SQUARE-0.5)
+THIRD_POINT = Pixel(create_point(randint(0,GRID-1),randint(0,GRID-1)),GREEN,SQUARE-0.5)
 
 #main loop
 while go:
@@ -82,21 +80,48 @@ while go:
     #create grid
     for i in range(GRID):
         for j in range(GRID):
-            pixel = Pixel(create_point(i,j),GREY, DISTANCE-1)
+            pixel = Pixel(create_point(i,j),GREY, SQUARE-1)
             pixel_list.append(pixel)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             go = False
 
-    rand = randint(-1,1)
-    FIRST_POINT.pos += (rand*DISTANCE,rand*DISTANCE)
+    if FIRST_POINT.x >= WIDTH-SQUARE:
+        FIRST_POINT.pos += (-SQUARE, rand*SQUARE)
+    elif FIRST_POINT.x <= SQUARE:
+        FIRST_POINT.pos += (SQUARE, rand*SQUARE)
+    elif FIRST_POINT.y >= HEIGHT-SQUARE:
+        FIRST_POINT.pos += (rand*SQUARE, -SQUARE)
+    elif FIRST_POINT.y <= SQUARE:
+        FIRST_POINT.pos += (rand*SQUARE, SQUARE)
+    else:
+        rand = randint(-1,1)
+        FIRST_POINT.pos += (rand*SQUARE,rand*SQUARE)
 
-    rand = randint(-1,1)
-    SECOND_POINT.pos += (rand*DISTANCE,rand*DISTANCE)
+    if SECOND_POINT.x >= WIDTH-SQUARE:
+        SECOND_POINT.pos += (-SQUARE, rand*SQUARE)
+    elif SECOND_POINT.x <= SQUARE:
+        SECOND_POINT.pos += (SQUARE, rand*SQUARE)
+    elif SECOND_POINT.y >= HEIGHT-SQUARE:
+        SECOND_POINT.pos += (rand*SQUARE, -SQUARE)
+    elif SECOND_POINT.y <= SQUARE:
+        SECOND_POINT.pos += (rand*SQUARE, SQUARE)
+    else:
+        rand = randint(-1,1)
+        SECOND_POINT.pos += (rand*SQUARE,rand*SQUARE)
 
-    rand = randint(-1,1)
-    THIRD_POINT.pos += (rand*DISTANCE,rand*DISTANCE)
+    if THIRD_POINT.x >= WIDTH-SQUARE:
+        THIRD_POINT.pos += (-SQUARE, rand*SQUARE)
+    elif THIRD_POINT.x <= SQUARE:
+        THIRD_POINT.pos += (SQUARE, rand*SQUARE)
+    elif THIRD_POINT.y >= HEIGHT-SQUARE:
+        THIRD_POINT.pos += (rand*SQUARE, -SQUARE)
+    elif THIRD_POINT.y <= SQUARE:
+        THIRD_POINT.pos += (rand*SQUARE, SQUARE)
+    else:
+        rand = randint(-1,1)
+        THIRD_POINT.pos += (rand*SQUARE,rand*SQUARE)
 
     #create lines between points with gradients
     first_second = colorise_line(bresenham_list(FIRST_POINT.pos,SECOND_POINT.pos),FIRST_POINT.color,SECOND_POINT.color)
---
+++ bresenham.py
@@ -0,0 +1,41 @@
+"""Implementation of Bresenham's line drawing algorithm
+
+See en.wikipedia.org/wiki/Bresenham's_line_algorithm
+"""
+
+WIDTH = 800
+GRID = 32
+DISTANCE = WIDTH/GRID
+
+
+def bresenham(x0, y0, x1, y1):
+    """Yield integer coordinates on the line from (x0, y0) to (x1, y1).
+
+    Input coordinates should be integers.
+
+    The result will contain both the start and the end point.
+    """
+    dx = x1 - x0
+    dy = y1 - y0
+
+    xsign = DISTANCE if dx > 0 else -DISTANCE
+    ysign = DISTANCE if dy > 0 else -DISTANCE
+
+    dx = abs(dx)
+    dy = abs(dy)
+
+    if dx > dy:
+        xx, xy, yx, yy = xsign, 0, 0, ysign
+    else:
+        dx, dy = dy, dx
+        xx, xy, yx, yy = 0, ysign, xsign, 0
+
+    D = 2*dy - dx
+    y = 0
+
+    for x in range(int(dx/DISTANCE) + 1):
+        yield x0 + x*xx + y*yx, y0 + x*xy + y*yy
+        if D >= 0:
+            y += 1
+            D -= 2*dx
+        D += 2*dy
